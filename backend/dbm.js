5060edc1540ad6320a8d4c66dab3cf42b48d04a4
Iganza-roy <iganzaroy55@gmail.com>
2024-07-03 23:12:39 +0300

    Update dbm.js

--- backend/dbm.js
+++ backend/dbm.js
@@ -1,8 +1,12 @@
-import mysql from 'mysql'
+// creating a connection with the database
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '#majoriganza5830',
-    database: 'litnerd'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
--- frontend/src/pages/Single.jsx
+++ frontend/src/pages/Single.jsx
@@ -52,7 +52,7 @@ const Single = () => {
             <span>{post.username}</span>
             <p>Posted {moment(post.date).fromNow()}</p>
           </div>
-          {currentUser.username === post.username && (<div className="edit">
+          {currentUser && currentUser.username === post.username && (<div className="edit">
             <Link to={`/write?edit=2`} state={post}>
               <img src= {Edit} alt="edit" />
             </Link>
