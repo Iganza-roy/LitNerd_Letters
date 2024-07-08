184d4ad8f1d7b447243bf9ce583577804b97f79f
Iganza-roy <iganzaroy55@gmail.com>
2024-07-08 12:31:50 +0300

    Update backend.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '../frontend/public/upload/')
+        cb(null, './public/upload/')
     },
     filename: function (req, file, cb) {
         cb(null, Date.now()+file.originalname)
