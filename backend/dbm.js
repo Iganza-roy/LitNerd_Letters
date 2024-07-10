6031279f8afba191da6c1e2601c4e3d93439606b
Iganza-roy <iganzaroy55@gmail.com>
2024-07-10 23:38:12 +0300

    Update dbm.js

--- backend/dbm.js
+++ backend/dbm.js
@@ -1,13 +1,13 @@
 // creating a connection with the database
-import mysql from 'mysql';
-import dotenv from 'dotenv';
+import mysql from "mysql2";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 export const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
