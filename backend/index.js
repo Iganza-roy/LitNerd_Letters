24b8bd9c2a75b9adb646f1cb5693a8c9890759a6
Iganza-roy <iganzaroy55@gmail.com>
2024-07-10 23:01:26 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
 // main entry point for the backend server
 
-import express from 'express';
+import express from "express";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
-import cookieParser from 'cookie-parser';
-import multer from 'multer';
-import cors from 'cors';
+import cookieParser from "cookie-parser";
+import multer from "multer";
+import cors from "cors";
 
 const app = express();
 
 // Enabling CORS for frontend domain
 const corsOptions = {
-    origin: 'https://litnerdletters.netlify.app',
+  origin: "https://litnerdletters.netlify.app",
 };
 
 app.use(cors(corsOptions));
@@ -22,22 +22,22 @@ app.use(cookieParser());
 // handling file upload with multer
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, '../frontend/public/upload/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now()+file.originalname)
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "../frontend/public/upload/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-const upload = multer({ storage })
+const upload = multer({ storage });
 
 // file upload route
 
-app.post('/api/upload', upload.single('file'), function(req, res) {
-    const file = req.file;
-    res.status(200).json(file.filename) // return the filename on successful upload
-})
+app.post("/api/upload", upload.single("file"), function (req, res) {
+  const file = req.file;
+  res.status(200).json(file.filename); // return the filename on successful upload
+});
 
 app.use("/api/auth", authRoutes);
 
@@ -45,9 +45,9 @@ app.use("/api/users", userRoutes);
 
 app.use("/api/posts", postRoutes);
 
+app.use("/", postRoutes);
 
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
-    console.log(`connected to Backend on port ${PORT}!`);
+  console.log(`connected to Backend on port ${PORT}!`);
 });
-
